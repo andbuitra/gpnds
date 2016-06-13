52c1fcef733311880fb1a72962b4b65d5dbb3b89
brendagelvez <brendagelvez@gmail.com>
2016-06-12 21:04:54 -0500

    Cambie lo de mision y vision, y cambie el panel de Admin
    Despues lo hago lo de mision y vision tambien en el de Web

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,32 +4,22 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 about-us-text wow fadeInLeft">
-				<h3>Sobre el Grupo de Puericultura</h3>
+				<h3>QUIENES SOMOS</h3>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
-					Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper <span class="violet">suscipit lobortis</span>
-					nisl ut aliquip ex ea commodo consequat. Lorem ipsum <strong>dolor sit amet</strong>, consectetur adipisicing elit,
-					sed do eiusmod tempor incididunt ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
-					suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-					sed do <strong>eiusmod tempor</strong> incididunt.
+					Somos un espacio de reflexión creado por un grupo interdisciplinario de profesionales, para el estudio, intercambio de saberes y generación de acuerdos sobre prácticas de crianza que propendan por el desarrollo adecuado, integral, diverso e incluyente de nuestros niños, niñas y adolescentes, con educación, cuidados, juego, nutrición, hábitos saludables y buen trato, quienes juntos descubrimos nuevas formas de hacer, sentir y pensar con la infancia.
+
+					Profundizamos en el discurso de crianza humanizada del Grupo de Puericultura de la Universidad de Antioquia, quienes durante más de 20 años han trabajando en pro de llevar a la comunidad el conocimiento científico de la Puericultura, ciencia que se encarga del estudio del cuidado de los niños, de la crianza.
+
 				</p>
 				<h3>Nuestra mision</h3>
 				<p>
-					Lorem ipsum dolor sit amet, <span class="violet">consectetur adipisicing</span> elit, sed do eiusmod tempor incididunt
-					ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip
-					ex ea commodo consequat. Lorem ipsum dolor sit amet, <strong>consectetur adipisicing</strong> elit, sed do eiusmod
-					<span class="violet">tempor incididunt</span> ut labore et. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur
-					adipisicing elit, sed do eiusmod tempor incididunt.
+					El Grupo de Puericultura de Norte de Santander, es un espacio de participación interdisciplinaria de personas que se unen con la intención de reflexionar, generar conocimiento,  construir acuerdos y  promover  acciones en torno a las prácticas de crianza que se consideran más adecuadas para el acompañamiento inteligente y afectuoso a los niños, niñas y adolescentes de nuestra región, en este apasionante proceso. 
+
+
 				</p>
 				<h3>Nuestra Vision</h3>
 				<p>
-					Lorem ipsum dolor sit amet, <strong>consectetur adipisicing elit</strong>, sed do eiusmod tempor incididunt ut labore et.
-					Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-					consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.
-					Ut wisi enim ad minim veniam, quis <span class="violet">nostrud exerci</span> tation ullamcorper suscipit lobortis
-					nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt.
+					El Grupo de Puericultura de Norte de Santander será un agente de apoyo para los profesionales, instituciones, padres y demás adultos significativos responsables de la crianza de los niños, niñas y adolescentes de nuestra región, que incidirá en la implementación de una cultura de niñez, que reconoce, valora y promueve el desarrollo integral y la garantía de los derechos de los niños y las niñas.
 				</p>
 			</div>
 		</div>
