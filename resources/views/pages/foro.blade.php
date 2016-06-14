29253c078efd8643153249884be399e8db06ced8
brendagelvez <brendagelvez@gmail.com>
2016-06-14 13:12:59 -0500

    Agregue boton de likes, y agregue un primer intento por ahora del foro
    Si el boton no es factible, se puede cambiar por otro que sea mas facil
    Si la tabla de usuarios no se arreglo aun, me avisan
    Por ahora aun estoy arreglando lo del foro, no es version final

---
+++ resources/views/pages/foro.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('includes.head')
+</head>
+<body>
+	<header>
+		@include('includes.header')
+	</header>
+
+	<div class="page-title-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 wow fadeIn">
+                        <i class="fa fa-pencil"></i>
+                        <h1>Foro /</h1>
+                        <p>Encuentra respuestas a temas comunes, y participa comentando.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="testimonials-container">
+	        <div class="container">
+	        	<div class="row">
+		            
+	            </div>
+	            <div class="row">
+				<div = class="col-sm-12 wow fadeInLeftBig">
+
+
+						<div class="question-section">
+						  <div class="question">
+						  
+
+						    <h4>PREGUNTA</h4>
+
+						    <div id="question-container">
+
+
+						      <div class="quest">
+						        <div class="quest-user">
+						          <div class="avatar"><img src="/assets/img/deadpool.jpg" alt="Riccardo"/></div><span class="user-details"><span class="username">Deadpool</span><span> on </span><span>MARCH 7, 2016</span></span>
+						        </div>
+						        <div class="quest-text">
+						           
+						          Its okay if my son starts to have a fever over 39°? Can I give him ice cream?<br><br>
+						          
+						        </div>
+						      </div>
+						  
+						   
+						    </div>
+						  </div>
+						  </div>
+
+						<div class="comments-section">
+						  <div class="comments">
+
+
+						    <h4>RESPUESTAS</h4>
+
+						    <div id="comments-container">
+
+
+						      <div class="comment">
+						        <div class="comment-user">
+						          <div class="avatar"><img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/49366/profile/profile-80_1.jpg" alt="Riccardo"/></div><span class="user-details"><span class="username">Riccardo </span><span> on </span><span>MARCH 7, 2016</span></span>
+						        </div>
+						        <div class="comment-text">
+						           
+						          Dude, worst dad ever
+						        </div>
+						      </div>
+						      <div class="comment">
+						        <div class="comment-user">
+						          <div class="avatar"><img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/49366/profile/profile-80_1.jpg" alt="Riccardo"/></div><span class="user-details"><span class="username">Riccardo </span><span> on </span><span>MARCH 7, 2016</span></span>
+						        </div>
+						        <div class="comment-text">
+						           
+						          What a horror
+						        </div>
+						      </div>
+						      <div class="comment">
+						        <div class="comment-user">
+						          <div class="avatar"><img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/49366/profile/profile-80_1.jpg" alt="Riccardo"/></div><span class="user-details"><span class="username">Riccardo </span><span>on </span><span>MARCH 7, 2016</span></span>
+						        </div>
+						        <div class="comment-text">
+						          Try actions like keyboard shortcuts for inline styling. Also try tagging someone! Maybe me @rkpasia :) <br><br>
+						          Have fun.<br>
+						          Be cool.
+						        </div>
+						      </div>
+						    </div>
+						  </div>
+						  <div class="comment-editor">
+						    <h4>DEJA TU RESPUESTA</h4>
+						    <div id="comment-form"></div>
+						  </div>
+						</div>
+
+					</div>
+	                
+	            </div>
+	        </div>
+        </div>
+        <script src="/assets/js/foro.js"></script>
+
+	
+	<!-- FIN CUERPO DE LA PÁGINA -->
+
+	<footer>
+		@include('includes.footer')
+	</footer>
+</body>
+</html>
--- resources/views/pages/single-post.blade.php
+++ resources/views/pages/single-post.blade.php
@@ -28,32 +28,70 @@
 					<div class="col-sm-8">
 						<div class="blog-post blog-single-post">
 							<div class="single-post-title">
-								<h2>{{$post->title}}</h2>
+								<h2>Ultimos cuidados con la alimentacion de ni&ntilde;os</h2>
 							</div>
 							<div class="single-post-info">
-								<i class="glyphicon glyphicon-time"></i>{{$post->created_at}} <a href="#" title="Show Comments"><i class="glyphicon glyphicon-comment"></i>11</a>
+								<i class="glyphicon glyphicon-time"></i>15 OCT, 2014 <a href="#" title="Show Comments"><i class="glyphicon glyphicon-comment"></i>11</a>
 							</div>
 							<div class="single-post-content" align="justify">
-								{{$post->body}}
-							</div>
-							<div class="bcomment">
-							<div class="comments">
-								<div class="comment-wrap">
-										<div class="photo">
-												<div class="avatar" style="background-image: url('{{Auth::user()->user_image_uri}}')"></div>
-										</div>
-										<div class="comment-block">
-												<form action="/nuevo/comentario" method="post">
-													{!! csrf_field() !!}
-														<textarea name="body" id="" cols="30" rows="3" placeholder="Add comment..."></textarea>
-														<input type="hidden" name="post_id" value="{{$post->post_id}}">
-														<button type="submit">COMMENT</button>
-												</form>
-										</div>
-								</div>
-						</div>
-						</div>
+								<img src="../assets/img/articulos/1.jpg" alt="Post Title">
+								<h3>Lorem ipsum dolor sit amet</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio. Duis elementum ligula eu ipsum condimentum accumsan.
+								</p>
+								<p>
+									Vivamus euismod elit ac libero facilisis tristique. Duis mollis non ligula vel tincidunt. Nulla consectetur libero id nunc ornare, vel vulputate tellus mollis. Sed quis nulla magna. Integer rhoncus sem quis ultrices lobortis. Maecenas tempus nulla quis dolor vulputate egestas. Phasellus cursus tortor quis massa faucibus fermentum vel sit amet tortor. Phasellus vehicula lorem et tortor luctus, a dignissim lacus tempor. Aliquam volutpat molestie metus sit amet aliquam. Duis vestibulum quam tortor, sed ultrices orci sagittis nec.
+								</p>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mattis, nulla id pretium malesuada, dui est laoreet risus, ac rhoncus eros diam id odio. Duis elementum ligula eu ipsum condimentum accumsan.
+								</p>
+								<p>
+									Vivamus euismod elit ac libero facilisis tristique. Duis mollis non ligula vel tincidunt. Nulla consectetur libero id nunc ornare, vel vulputate tellus mollis. Sed quis nulla magna. Integer rhoncus sem quis ultrices lobortis. Maecenas tempus nulla quis dolor vulputate egestas. Phasellus cursus tortor quis massa faucibus fermentum vel sit amet tortor. Phasellus vehicula lorem et tortor luctus, a dignissim lacus tempor. Aliquam volutpat molestie metus sit amet aliquam. Duis vestibulum quam tortor, sed ultrices orci sagittis nec.
+								</p>
+								<h3>Sed sit amet metus sit</h3>
+								<p>
+									Proin fermentum, purus id eleifend molestie, nisl arcu rutrum tellus, a luctus erat tortor ut augue. Vivamus feugiat nisi sit amet dignissim fermentum. Duis elementum mattis lacinia. Sed sit amet metus sit amet leo semper feugiat. Nulla vel orci nec neque interdum facilisis egestas vitae lorem. Phasellus elit ante, posuere at augue quis, porta vestibulum magna. Nullam non mattis odio. Donec eget velit leo. Nunc et diam volutpat tellus ultrices fringilla eu a neque. Integer lectus nunc, vestibulum a turpis vitae, malesuada lacinia nibh. In sit amet leo ut turpis convallis bibendum. Nullam nec purus sapien. Quisque sollicitudin cursus felis sit amet aliquam.
+								</p>
 
+							</div>
+
+							<div class="col-sm-12">
+								
+								
+									<div class="post">
+									  <header>								    
+									    
+									  </header>
+									  <p>No olvides darle me gusta.</p>
+									  <a href="#" class="like">&#10084;</a>
+									</div>
+								
+							</div>
+
+							
+							<div id="disqus_thread"></div>
+								<script>
+									/**
+									* RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+									* LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+									*/
+									/*
+									var disqus_config = function () {
+									this.page.url = PAGE_URL; // Replace PAGE_URL with your page's canonical URL variable
+									this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+									};
+									*/
+									(function() { // DON'T EDIT BELOW THIS LINE
+									var d = document, s = d.createElement('script');
+
+									s.src = '//gpndschat.disqus.com/embed.js';
+
+									s.setAttribute('data-timestamp', +new Date());
+									(d.head || d.body).appendChild(s);
+									})();
+								</script>
+								<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+		
 						</div>
 
 
@@ -114,19 +152,19 @@
 
 
 	    </div>
-
+	    
 
 
         <!-- Javascript -->
-        <script src="/assets/js/comment.js"></script>
         <script src="/assets/js/jquery-1.11.1.min.js"></script>
+        
         <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
         <script src="/assets/js/bootstrap-hover-dropdown.min.js"></script>
         <script src="/assets/js/jquery.backstretch.min.js"></script>
         <script src="/assets/js/wow.min.js"></script>
-        <script src="/assets/js/retina-1.1.0.min.js"></script>
-        <script src="/assets/js/jquery.magnific-popup.min.js"></script>
-        <script src="/assets/flexslider/jquery.flexslider-min.js"></script>
+        <script src="assets/js/retina-1.1.0.min.js"></script>
+        <script src="assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="assets/flexslider/jquery.flexslider-min.js"></script>
         <script src="assets/js/jflickrfeed.min.js"></script>
         <script src="assets/js/masonry.pkgd.min.js"></script>
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
