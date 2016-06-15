906c33ba0afa154fc9a6bfc0ccb6ac60951f407d
AndrezNinno <andrezlokomotion7@gmail.com>
2016-06-15 00:20:21 -0500

    Algo

--- public/assets/js/forum.js
+++ public/assets/js/forum.js
@@ -96,11 +96,7 @@ jQuery(document).ready(function($) {
 	// Enable sexy scrollbars
 	$('.nano').nanoScroller();
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 789ae96fde71a82c7675581defeb62c766117875
+	
 	// Search box responsive stuff
 
 	$('.search-box input').on('focus', function() {
